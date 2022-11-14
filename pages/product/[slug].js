a05091ed51a2f904616a3d3889cf9901dbf61e41
orginalsk01 <sipssman@gmail.com>
2022-11-14 21:02:00 +0200

    Small error fix

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -21,7 +21,9 @@ const ProductDetails = ({ product, products }) => {
                     </div>
                     <div className='small-images-container'>
                         {image?.map((item, i) => (
-                            <img src={urlFor(item)}
+                            <img 
+                            key={i}
+                            src={urlFor(item)}
                             className={i == index ? 'small-image selected-image' : 'small-image'}
                             onMouseEnter={() => setIndex(i)}/>
                         ))}
